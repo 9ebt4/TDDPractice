a8be4a208c4ae403a8d4e5b7cd4692a7b585fa32
ethan <ethanbjansen@gmail.com>
2024-08-27 12:57:52 -0500

    Practicing TDD

---
+++ TDDTesting.test/CalculatorTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TDD_testing;
+
+namespace TDDTesting.test
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Addition_ReturnsCorrectSum()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Add(2, 3);
+            Assert.Equal(5, result);
+        }
+        [Fact]
+        public void Subtraction_ReturnCorrectDifference()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Subtract(2, 3);
+            Assert.Equal(-1, result);
+        }
+        [Fact]
+        public void Multiplication_ReturnCorrectProduct()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Multiply(2, 3);
+            Assert.Equal(6, result);
+        }
+        [Fact]
+        public void Divider_ReturnCorrectQuotient()
+        {
+            var calculator = new Calculator();
+            var result = calculator.Divide(3, 3);
+            Assert.Equal(1, result);
+        }
+    }
+}
